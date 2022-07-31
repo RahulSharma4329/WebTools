7bab3ee03be7631f8f4183f1611d1e64f3676c66
Rahulsharma4329 <rahulsharma4329@gmail.com>
2022-08-01 02:03:08 +0530

    2nd commi t

---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./navcss.css"
+
+export default function Navbar() {
+  return (
+    <div>
+      <div className="navcontainer">
+        <div className="logo">
+            <img src="/logo512.png" alt=""/>
+            <p>Tiny Web Tools</p>
+        </div>
+
+      </div>
+    </div>
+  )
+}
